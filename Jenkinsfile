9a1add7103945a98a20fb8b6c0617e614e8ef6fc
taili87 <ablayethiam87@hotmail.com>
2023-03-02 15:10:19 -0500

    jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,27 +1,37 @@
+pipeline{
+	agent any 
+	parameters{
+		
+			string(name: 'SPEC', 
+			defaultValue: 'cypress/e2e/**/**', 
+			description:'Enter the scripts path that you want execute')
 
+			 choice(name:'BROWSER',
+			 choices: ['chrome', 'edge','firefox'],
+			 description: 'choise the browser where you want to tun test')
+	}
 
-pipeline {
-	agent any
-	stages {
-		stage('Clone Git Repo'){
-				steps{
-					git 'https://github.com/taili87/cypress-e2e.git'
-		    }
+		options{
+			ansiColor('xterm')
 		}
-		stage('Install Dependencies'){
+
+		stages{
+			stage('Deploying app'){
+				echo 'Building the Application'
+			}
+			stage('Testing'){
 				steps{
 					bat 'npm install'
+					bat 'npx cypress run --browser ${BROWSER} --spec ${SPEC}'
+
 				}
-		}
-		stage('Run Tests'){
-				steps{
-					bat 'npm test'
+			}
+
+			post{
+				always {
+					publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'cypress/reports/html', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
 				}
-		}
-		stage('Publish HTML Report'){
-				steps{
-					publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'cypress/reports/', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
-				}
+			}
 		}
 	}
-}
+	
--- cypress/e2e/test-demo/dropDown.cy.js
+++ cypress/e2e/test-demo/dropDown.cy.js
@@ -3,14 +3,14 @@
 
 describe('Drop down Test', () =>{
 
-    it.skip('Static drop down with select', ()=>{
+    it('Static drop down with select', ()=>{
           cy.visit("https://www.zoho.com/commerce/free-demo.html");
           cy.get("#zcf_address_country")
           .select('Antarctica')
           .should('have.value', 'Antarctica');
     })
 
-    it.skip('Bootstrap drop down without select tag', ()=>{
+    it('Bootstrap drop down without select tag', ()=>{
         cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application/");
         cy.get("#select2-billing_country-container").click();
         cy.get('.select2-search__field').type('Iran').type('{enter}');
@@ -19,7 +19,7 @@ describe('Drop down Test', () =>{
         cy.get("#select2-billing_country-container").should('have.text', 'Iran');
   })
 
-  it.skip('Auto suggest dropDown', ()=>{
+  it('Auto suggest dropDown', ()=>{
     cy.visit("https://www.wikipedia.org/");
     cy.get("#searchInput").type('Delhi');
     cy.get('.suggestion-title').contains('Delhi University').click();
@@ -29,16 +29,17 @@ describe('Drop down Test', () =>{
 
 it('Dynamic dropDown', ()=>{
     cy.visit("https://www.google.com/");
+    
     cy.get("input[name='q']").type('cypress automation tutorial');
-    cy.wait(3000);
-    cy.get("div.wM6W7d>span").should('have.length', 11)
+    cy.wait(5000);
+    cy.get("div.wM6W7d>span").should('have.length', 12)
     cy.get("div.wM6W7d>span").each(($element, index, $list) =>{
         if($element.text() == 'cypress automation tutorial'){
             cy.wrap($element).click();
         }else{
             console.log('Element not found !')
         }
-        cy.get('#rso > :nth-child(2) > .MjjYud > .g > .kvH3mc > .jGGQ5e > .yuRUbf > a > .LC20lb').should('be.visible');
+        cy.get('.jfN4p').should('be.visible');
     })
    
  
